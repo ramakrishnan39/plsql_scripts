75fd74e8bcced80dfad7001964c8ec8f3f8d56f6
Ramakrishnan <52151056+ramakrishnan39@users.noreply.github.com>
2021-06-27 19:32:43 +0530

    commit on master

--- hrdata.sql
+++ hrdata.sql
@@ -118,18 +118,6 @@ values(
  1300, null, 10
 );
  
-/*
-insert into salgrade
-values (1, 700, 1200);
-insert into salgrade
-values (2, 1201, 1400);
-insert into salgrade
-values (3, 1401, 2000);
-insert into salgrade
-values (4, 2001, 3000);
-insert into salgrade
-values (5, 3001, 9999);
-*/
  
 COMMIT;
 
